4d92f7105ffdbd4bcf78b87edf450f6469efec49
Andrei Priboi <123972689+andreiprb@users.noreply.github.com>
2025-08-11 16:58:18 +0300

    Delete datahandler directory

--- datahandler/processors/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Text processors package.
-"""
-
-from .base import BaseProcessor
-from .english import EnglishProcessor
-from .romanian import RomanianProcessor
-
-__all__ = ['BaseProcessor', 'EnglishProcessor', 'RomanianProcessor']
